a452b82b32de5303c6bbb9a2ece5f715b93caf18
Charles Oder <charles@oder.us>
2015-04-02 06:14:30 -0500

    Problem 2: Added method to calculate Fibonacci Sequence using a loop

---
+++ ProjectEuler.Tests/FibonacciTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [Test]
+        public void FibonacciSequenceIsCorrectFOrFirst10()
+        {
+            var expectedArray = new int[] {1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+            var actualArray = Fibonacci.GetSequence(10);
+            Assert.IsTrue(expectedArray.SequenceEqual(actualArray), "Arrays do not match!");
+        }
+    }
+}
---
+++ ProjectEuler/Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler {
+    public static class Fibonacci {
+        public static IEnumerable<int> GetSequence(int count)
+        {
+            var output = new List<int>();
+            var previousValues = new [] {0, 1};
+            for (var i = 0; i < count; i++)
+            {
+                var nextValue = previousValues[0] + previousValues[1];
+                output.Add(nextValue);
+                previousValues[0] = previousValues[1];
+                previousValues[1] = nextValue;
+            }
+            return output;
+        }
+    }
+}
