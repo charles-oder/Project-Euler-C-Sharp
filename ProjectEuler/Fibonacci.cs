ca456d5db257f457b8ff2940f041e1c0baf9a71c
Charles Oder <charles@oder.us>
2015-04-02 06:39:53 -0500

    Problem 2: Added method to calculate all Fibonacci Numbers under a maximum value

--- ProjectEuler/Fibonacci.cs
+++ ProjectEuler/Fibonacci.cs
@@ -19,5 +19,19 @@ namespace ProjectEuler {
             }
             return output;
         }
+
+        public static IEnumerable<int> GetSequenceForMax(int max)
+        {
+            var output = new List<int>();
+            var previousValues = new[] { 0, 1 };
+            var nextValue = 1;
+            while (nextValue < max) {
+                output.Add(nextValue);
+                previousValues[0] = previousValues[1];
+                previousValues[1] = nextValue;
+                nextValue = previousValues[0] + previousValues[1];
+            }
+            return output;
+        }
     }
 }
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ProjectEuler {
     class Program {
         static void Main(string[] args)
         {
-            var x = Multiples.Sum(new List<int>{3, 5}, 1000);
-            Console.Out.WriteLine(string.Format("The Multiples of 3 and 5 under 1000 is {0}", x ));
+            var evenFibSeq = ArrayFilter.FilterByLambda(Fibonacci.GetSequence(20),x => x%2 == 0);
+            Console.Out.WriteLine(string.Format("The sum of even Fibonacci NUmbers are {0}", evenFibSeq.Sum() ));
             Console.ReadLine();
         }
     }
