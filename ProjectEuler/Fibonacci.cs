0d4ac0cf95b953bb117be7a53272fc007b2418ea
Charles Oder <charles.oder@emerson.com>
2015-04-02 07:57:36 -0500

    Problem 2: Refactored generating sequences to a max value to the builder pattern

--- ProjectEuler/Fibonacci.cs
+++ ProjectEuler/Fibonacci.cs
@@ -1,28 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ProjectEuler {
     public class Fibonacci {
         private int count;
+        public int Max { get; set; }
 
         public Fibonacci(int count)
         {
             this.count = count;
+            Max = int.MaxValue;
         }
+
+        public Fibonacci() : this(int.MaxValue) {}
 
         public IEnumerable<int> GetSequence()
         {
             var output = new List<int>();
             var previousValues = new[] { 0, 1 };
-            for (var i = 0; i < count; i++)
+            var nextValue = previousValues[1];
+            while (nextValue < Max && output.Count < count)
             {
-                var nextValue = previousValues[0] + previousValues[1];
                 output.Add(nextValue);
                 previousValues[0] = previousValues[1];
                 previousValues[1] = nextValue;
+                nextValue = previousValues[0] + previousValues[1];
             }
             return output;
         } 
