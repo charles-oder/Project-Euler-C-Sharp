92dd51379abcacdfdbd44ef95c5bdfaff486f450
Charles Oder <charles@oder.us>
2015-04-04 09:28:36 -0500

    Problem 4: Added method to split an int into a list of digits

---
+++ ProjectEuler.Tests/PalindromeTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class PalindromeTests
+    {
+
+        [Test]
+        public void GetDigitList()
+        {
+            const int testValue = 123456789;
+            var expectedList = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            var palindromeDigitList = Palindrome.CreateDigitList(testValue);
+
+            Assert.AreEqual( expectedList, palindromeDigitList);
+        }
+
+    }
+}
---
+++ ProjectEuler/Palindrome.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    public class Palindrome
+    {
+        public static List<int> CreateDigitList(int value)
+        {
+            var list = new List<int>();
+            while (value > 0)
+            {
+                var digit = value%10;
+                list.Add(digit);
+                value /= 10;
+            }
+            list.Reverse();
+            return list;
+        }
+
+    }
+}
