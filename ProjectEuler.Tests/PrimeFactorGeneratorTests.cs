4d3983065af68251d689a513e04acfc86ce665f7
Charles Oder <charles@oder.us>
2015-04-04 08:28:01 -0500

    Problem 3: Added method to check if a number is prime

---
+++ ProjectEuler.Tests/PrimeFactorGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class PrimeFactorGeneratorTests
+    {
+
+        [Test]
+        public void IsPrimeWithPrimeNumbers() {
+            Assert.IsTrue(PrimeFactorGenerator.IsPrime(3));
+            Assert.IsTrue(PrimeFactorGenerator.IsPrime(5));
+            Assert.IsTrue(PrimeFactorGenerator.IsPrime(7));
+            Assert.IsTrue(PrimeFactorGenerator.IsPrime(11));
+            Assert.IsTrue(PrimeFactorGenerator.IsPrime(17));
+        }
+
+        [Test]
+        public void IsPrimeWithNonPrimeNumbers() {
+            Assert.IsFalse(PrimeFactorGenerator.IsPrime(1));
+            Assert.IsFalse(PrimeFactorGenerator.IsPrime(6));
+            Assert.IsFalse(PrimeFactorGenerator.IsPrime(55));
+            Assert.IsFalse(PrimeFactorGenerator.IsPrime(100));
+            Assert.IsFalse(PrimeFactorGenerator.IsPrime(30000000));
+        }
+    }
+}
---
+++ ProjectEuler/PrimeFactorGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class PrimeFactorGenerator
+    {
+        public static bool IsPrime(int value)
+        {
+            for (var i = 2; i <= Math.Sqrt(value); i++)
+            {
+                if (value%i == 0)
+                {
+                    return false;
+                }
+            }
+            return (value != 1);  // 1 is not a prime number
+        }
+    }
+}
