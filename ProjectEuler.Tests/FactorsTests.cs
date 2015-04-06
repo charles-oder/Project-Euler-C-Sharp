c73b3e95baa3193f73a3a6a86062a94257fdf432
Charles Oder <charles@oder.us>
2015-04-05 20:19:16 -0500

    Problem 5: Added method for calculating common dividend

---
+++ ProjectEuler.Tests/FactorsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests {
+    [TestFixture]
+    class FactorsTests {
+
+        [Test]
+        public void GenerateProductOfFactorsOneThroughTen()
+        {
+            var expectedValue = 2520;
+            var actualValue = Factors.GenerateSmallestDividendForSequence(1, 10);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
---
+++ ProjectEuler/Factors.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler {
+    public class Factors {
+        public static int GenerateSmallestDividendForSequence(int sequenceStart, int sequenceEnd)
+        {
+            var intList = new List<int>();
+            for (int i = sequenceStart; i <= sequenceEnd; i++)
+            {
+                intList.Add(i);
+            }
+            return GenerateSmallestDividendForSequence(intList);
+        }
+
+        private static int GenerateSmallestDividendForSequence(List<int> intList)
+        {
+            var increment = intList[intList.Count - 1];
+            for (var output = increment; output < int.MaxValue; output += increment)
+            {
+                if (IsDivisibleByAllValues(output, intList))
+                {
+                    return output;
+                }
+            }
+            return 0;
+        }
+
+        private static bool IsDivisibleByAllValues(int value, List<int> intList)
+        {
+            foreach (var i in intList)
+            {
+                if (value%i != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
